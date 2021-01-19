69332cc2b176b743eadbaba2ebbbc402898951fa
Slava Kostenko <shegan782@gmail.com>
2021-01-19 14:06:54 +0200

    1

--- src/application.js
+++ src/application.js
@@ -1,5 +1,9 @@
 import 'bootstrap/dist/css/bootstrap-grid.min.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import axios from 'axios';
+
+const proxy = 'hexlet-allorigins.herokuapp.com';
+const hexlet = 'https://ru.hexlet.io/lessons.rss'
 
 export default () => {
   const wrapper = document.createElement('div');
@@ -15,4 +19,16 @@ export default () => {
   buttonWrapper.appendChild(button);
   wrapper.append(input, button);
   document.body.appendChild(wrapper);
-}
+
+  axios.get(hexlet, {
+    proxy: {
+      host: 'https://hexlet-allorigins.herokuapp.com',
+      port: '1458'
+    }
+  }).then((r) => {
+    const parser = new DOMParser();
+    const data = parser.parseFromString(r.request.response, 'text/xml')
+    document.body.innerHTML = data
+    console.log(data.getElementsByTagName('channel'));
+  })
+};
--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
 import app from './application.js';
-import bootstrap from 'bootstrap';
 
 app();
